b998faafd33a79712c90625d0c0f8807e13b161d
craxo-collab <amegharamban@gmail.com>
2025-08-25 21:52:52 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~wvcTxYAR#YY5Amn3Zs9pYijDCDkK0udg6Vk6SAxeVwFCY5ua6usg",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
